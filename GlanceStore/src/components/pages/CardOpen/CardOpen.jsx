89be48586d3de598a2e45c5baa690be6d9b28fe4
BarryTheAllen <radyga936@gmail.com>
2025-09-10 16:36:04 +0400

    upd

--- GlanceStore/src/components/pages/CardOpen/CardOpen.jsx
+++ GlanceStore/src/components/pages/CardOpen/CardOpen.jsx
@@ -1,29 +1,58 @@
 import { useLocation, useNavigate } from 'react-router';
-import { useCartActions } from '../../../hooks/cart/useCartActions';
 import styles from "./cardOpen.module.css"
 import AddToCartBtn from '../../UI/AddToCartBtn/AddToCartBtn';
+import { useState } from 'react';
 
 const CardOpen = () => {
+
+    const [activeImg, setActiveImg] = useState(0);
+    const [fading, setFading] = useState(false);
+
+    const handleImgChange = (newIndex) => {
+    if (newIndex === activeImg) return;
+
+    setActiveImg(newIndex);
+    
+    setFading(true);
+
+    setTimeout(() => {
+        setActiveImg(newIndex);
+        setFading(false);
+    }, 100);
+    };
 
     const location = useLocation();
 
     const navigate = useNavigate();
-
-    const { handleCart } = useCartActions();
     
     const { product} = location.state;
 
+
+
     return (
-              <div className={styles.container}>
+        <div className={styles.container}>
                 <div className={styles.block}>
                   <button onClick={() => navigate(-1)} className={styles.backlink}>
                       ← Назад
                   </button>
-                      <img 
-                          src={product.techImg[0]} 
+                  <div className={styles.img__wrapper}>
+                   <div className={styles.img__iner__wrapper}>
+                    {product.techImg.map((imgSrc, index) => (
+                        <img
+                        key={index}
+                        src={imgSrc}
+                        alt={product.title}
+                        className={styles.img__prewatch}
+                        onClick={() => handleImgChange(index)}
+                        />
+                    ))}
+                    </div>
+                    <img 
+                          src={product.techImg[activeImg]} 
                           alt={product.title}
-                          className={styles.img}
+                          className={`${styles.img} ${fading ? styles.imgFade : ''}`}
                       />
+                  </div>
                 </div>
                 <div className={styles.block2}>
                 <h1 className={styles.title}>{product.title}</h1>
